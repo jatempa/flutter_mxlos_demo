5693f561dbfd4eb879540f1e5faa1275c3abaa3e
Jorge Atempa <atempaitm@gmail.com>
2024-02-17 15:38:24 -0800

    Create 'user' model class

---
+++ lib/models/user.dart
@@ -0,0 +1,18 @@
+class User {
+  final String firstName;
+  final String lastName;
+  final String email;
+
+  User(this.firstName, this.lastName, this.email);
+
+  User.fromJson(Map<String, dynamic> json)
+      : firstName = json['firstName'] as String,
+        lastName = json['lastName'] as String,
+        email = json['email'] as String;
+
+  Map<String, dynamic> toJson() => {
+        'firstName': firstName,
+        'lastName': lastName,
+        'email': email,
+      };
+}
