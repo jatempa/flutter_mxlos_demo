75a51a9381bd0e4e0bb462fc8c14db18ae8304c0
Jorge Atempa <atempaitm@gmail.com>
2024-02-17 13:59:38 -0800

    Create custom form widget

---
+++ lib/custom_form.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+
+import 'package:myapp/utils/ui_helpers.dart';
+import 'package:myapp/widgets/base_text_form_field_widget.dart';
+
+class CustomForm extends StatefulWidget {
+  const CustomForm({super.key});
+
+  @override
+  State<CustomForm> createState() => _CustomFormState();
+}
+
+class _CustomFormState extends State<CustomForm> {
+  final _labelFirstName = "Nombre", _labelLastName = "Apellido";
+  final _labelStreetName = "Calle", _labelNoInt = "No Interior";
+  final _labelNoExt = "No Exterior", _labelZipCode = "CP";
+  String? _firstName, _lastName, _streetName, _noInt, _noExt, _zipCode;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        buildTextFormField(
+          label: _labelFirstName,
+          initialValue: _firstName,
+          onChanged: (value) => {
+            setState(() {
+              _firstName = value;
+            })
+          }
+        ),
+        UIHelper.verticalSpaceSmall,
+        buildTextFormField(
+          label: _labelLastName,
+          initialValue: _lastName,
+          onChanged: (value) => {
+            setState(() {
+              _lastName = value;
+            })
+          }
+        ),
+        UIHelper.verticalSpaceSmall,
+        buildTextFormField(
+          label: _labelStreetName,
+          initialValue: _streetName,
+          onChanged: (value) => {
+            setState(() {
+              _streetName = value;
+            })
+          }
+        ),        
+        UIHelper.verticalSpaceSmall,
+        Row(
+          children: <Widget>[
+            Expanded(
+              child: buildTextFormField(
+                label: _labelNoExt,
+                initialValue: _noExt,
+                maxLength: 6,
+                onChanged: (value) {
+                  if(value !=null && value.length <= 6) {
+                    setState(() => _noExt = value);
+                  }
+                } 
+              ),
+            ),
+            UIHelper.horizontalSpaceSmall,
+            Expanded(
+              child: buildTextFormField(
+                label: _labelNoInt,
+                initialValue: _noInt,
+                maxLength: 6,
+                onChanged: (value) {
+                  if(value !=null && value.length <= 6) {
+                    setState(() => _noInt = value);
+                  }
+                } 
+              ),
+            ),
+            UIHelper.horizontalSpaceSmall,
+            Expanded(
+              child: buildTextFormField(
+                label: _labelZipCode,
+                initialValue: _zipCode,
+                maxLength: 5,
+                onChanged: (value) {
+                  if(value !=null && value.length <= 5) {
+                    setState(() => _zipCode = value);
+                  }
+                } 
+              ),
+            ),
+          ]
+        )       
+      ],
+    );
+  }
+
+  Widget buildTextFormField({
+    String? initialValue,
+    String? label,
+    Function? onChanged,
+    TextInputType? keyboardType,
+    int? maxLength
+  }) {
+    return BaseTextFormFieldWidget(
+      initialValue: initialValue,
+      border: const OutlineInputBorder(),
+      keyboardType: keyboardType,
+      label: label,
+      onChanged: onChanged,
+      maxLength: maxLength,
+    );
+  }
+}
--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:toggle_switch/toggle_switch.dart';
+
+import 'package:myapp/custom_form.dart';
+import 'package:myapp/utils/ui_helpers.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
@@ -10,13 +14,9 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  final String _label_use_existing_data = "Usar Datos del Registro";
+  int? _selectedIndex = 0;
+  bool fillUserData = false;
 
   @override
   Widget build(BuildContext context) {
@@ -25,25 +25,41 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+      body: Container(
+        alignment: Alignment.center,
+        margin: EdgeInsets.all(12),
+        child: ListView(
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text(_label_use_existing_data),
+                    ToggleSwitch(
+                      initialLabelIndex: _selectedIndex,
+                      iconSize: 1,
+                      fontSize: 14,
+                      minHeight: 30,
+                      minWidth: 40,
+                      labels: ['No', 'Sí'],
+                      inactiveBgColor: Colors.white,
+                      activeBgColor: [Colors.green],
+                      onToggle: (index) {
+                        setState(() => _selectedIndex = index);
+                      }                      
+                    )                    
+                  ],
+                ),
+                UIHelper.verticalSpaceMedium,
+                const CustomForm()
+              ],
+            )
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      )
     );
   }
 }
