e5ab2fd7d35a38c70f3d957eca667a2140d41b58
Jorge Atempa <atempaitm@gmail.com>
2024-02-18 13:29:11 -0800

    Call search delegate widget from app bar

--- lib/home.dart
+++ lib/home.dart
@@ -4,7 +4,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 import 'package:myapp/models/user.dart';
+import 'package:myapp/user_details.dart';
 import 'package:myapp/user_list.dart';
+import 'package:myapp/widgets/users_data_search.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
@@ -49,6 +51,26 @@ class _MyHomePageState extends State<MyHomePage> {
             fontWeight: FontWeight.bold
           )
         ),
+        actions: <Widget>[
+          IconButton(
+            onPressed: () async {
+              final User? selectedUser = await showSearch<User>(
+                context: context,
+                delegate: UsersDataSearch(users: users)
+              );
+
+              if (selectedUser != null) {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => UserDetailsPage(user: selectedUser),
+                  ),
+                );
+              }
+            },
+            icon: const Icon(Icons.search, color: Colors.white)
+          )
+        ],
       ),
       body: Container(
         alignment: Alignment.center,
