180781b04fdce67fe2c3dd9e7a73ff0d2e27698c
Jorge Atempa <atempaitm@gmail.com>
2024-02-17 18:39:42 -0800

    Create 'constants' ui file

---
+++ lib/utils/ui_constants.dart
@@ -0,0 +1,8 @@
+const labelUseExistingData = "Usar Datos del Registro";
+const labelFirstName = "Nombre";
+const labelLastName = "Apellido";
+const labelStreetName = "Calle";
+const labelNoInt = "No Interior";
+const labelNoExt = "No Exterior";
+const labelZipCode = "CP";
+const labelEmail = "Email";
